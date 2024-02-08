c82b7a3baf7251db91c6d66561f00e72128e7f4c
Oren <oyomtov@fireblocks.com>
2024-02-08 11:40:43 +0200

    Update Web3 version in python test scripts

--- test/scripts/test-ipc.py
+++ test/scripts/test-ipc.py
@@ -3,7 +3,7 @@ from web3 import Web3
 
 web3 = Web3(Web3.IPCProvider(sys.argv[1], 60000))
 
-print("block number: ", web3.eth.block_number)
+print("block number: ", web3.eth.blockNumber)
 
-address = Web3.to_checksum_address("0x000095E79eAC4d76aab57cB2c1f091d553b36ca0")
-print("account balance: ", web3.from_wei(web3.eth.get_balance(address), "ether"))
+address = Web3.toChecksumAddress("0x000095E79eAC4d76aab57cB2c1f091d553b36ca0")
+print("account balance: ", web3.fromWei(web3.eth.getBalance(address), "ether"))
